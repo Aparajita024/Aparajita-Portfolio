3e2c3573acd2f88f6be06d814008536f44b5c681
Aparajita024 <aparajita2419@gmail.com>
2025-08-29 01:28:13 +0530

    Add menu button toggle functionality

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function time () {
+function time() {
     let time = new Date()
     let hours = time.getHours()
     let min = time.getMinutes()
     let sec = time.getSeconds()
 
-    hours = hours < 10 ? '0' + hours : hours 
+    hours = hours < 10 ? '0' + hours : hours
     min = min < 10 ? '0' + min : min
     sec = sec < 10 ? '0' + sec : sec
 
@@ -13,3 +13,14 @@ function time () {
 
 time()
 setInterval(time, 1000)
+
+const menuButton = document.querySelector('.menu-button');
+const menuIcon = document.querySelector('.menu-icon');
+const crossIcon = document.querySelector('.cross-icon');
+const menuList = document.querySelector('.menu-list');
+
+menuButton.addEventListener('click', () => {
+  menuIcon.classList.toggle('hidden');
+  crossIcon.classList.toggle('hidden');
+  menuList.classList.toggle('hidden');
+});
